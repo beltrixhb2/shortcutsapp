d32ca2fc2e0fcb07bc03e19eca838e6e522f68e5
Beltran Hidalgo <beltran_hidalgo@bnetbuilders.com>
2025-06-16 20:17:07 +0200

    Logging added closes #6

--- src/server.js
+++ src/server.js
@@ -2,9 +2,35 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
+const winston = require("winston");
 
 dotenv.config();
 const app = express();
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({ format: winston.format.json() }),
+  ],
+});
+
+app.use((req, res, next) => {
+  const logEntry = {
+    severity: "INFO",
+    time: new Date().toISOString(),
+    method: req.method,
+    url: req.originalUrl,
+    headers: req.headers,
+    body: req.body,
+    ip: req.ip,
+  };
+  logger.info(logEntry);
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
